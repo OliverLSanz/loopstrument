eb16b57386c88e4dd80335d3842ce7547db69caf
OliverLSanz <oliverlsanz@gmail.com>
2022-07-15 18:56:07 +0200

    Initial commit
    This commit contains the boilerplate code Bitwig generates when
    creating a new project.

---
+++ LinnstrumentLooping.control.js
@@ -0,0 +1,72 @@
+loadAPI(17);
+
+// Remove this if you want to be able to use deprecated methods without causing script to stop.
+// This is useful during development.
+host.setShouldFailOnDeprecatedUse(true);
+
+host.defineController("Roger Linn Design", "LinnstrumentLooping", "0.1", "e72dbb9b-ba1c-405e-998e-6a96dee48830", "OliverLSanz");
+
+host.defineMidiPorts(1, 1);
+
+if (host.platformIsWindows())
+{
+   // TODO: Set the correct names of the ports for auto detection on Windows platform here
+   // and uncomment this when port names are correct.
+   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0"], ["Output Port 0"]);
+}
+else if (host.platformIsMac())
+{
+   // TODO: Set the correct names of the ports for auto detection on Mac OSX platform here
+   // and uncomment this when port names are correct.
+   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0"], ["Output Port 0"]);
+}
+else if (host.platformIsLinux())
+{
+   // TODO: Set the correct names of the ports for auto detection on Linux platform here
+   // and uncomment this when port names are correct.
+   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0"], ["Output Port 0"]);
+}
+
+function init() {
+   transport = host.createTransport();
+   host.getMidiInPort(0).setMidiCallback(onMidi0);
+   host.getMidiInPort(0).setSysexCallback(onSysex0);
+
+   // TODO: Perform further initialization here.
+   println("LinnstrumentLooping initialized!");
+}
+
+// Called when a short MIDI message is received on MIDI input port 0.
+function onMidi0(status, data1, data2) {
+   // TODO: Implement your MIDI input handling code here.
+}
+
+// Called when a MIDI sysex message is received on MIDI input port 0.
+function onSysex0(data) {
+   // MMC Transport Controls:
+   switch (data) {
+      case "f07f7f0605f7":
+         transport.rewind();
+         break;
+      case "f07f7f0604f7":
+         transport.fastForward();
+         break;
+      case "f07f7f0601f7":
+         transport.stop();
+         break;
+      case "f07f7f0602f7":
+         transport.play();
+         break;
+      case "f07f7f0606f7":
+         transport.record();
+         break;
+   }
+}
+
+function flush() {
+   // TODO: Flush any output to your controller here.
+}
+
+function exit() {
+
+}
